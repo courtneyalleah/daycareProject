06b78dfc48fe9756434b3228717f75fd0c70f2bd
Courtney <csuper95@gmail.com>
2018-10-15 16:47:56 -0400

    setting up layout ideas

---
+++ src/Homepage/HomePage.js
@@ -0,0 +1,12 @@
+import React, { Components } from 'react';
+
+class HomePage extends React.Component {
+ 
+  render() {
+    return(
+      <div>
+        <button>Hello</button>;
+      </div>
+    ); 
+  }
+}
