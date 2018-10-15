625cc1e4eb3fd54d1a7c5690c9720984b3e240b8
Courtney <csuper95@gmail.com>
2018-10-15 18:50:21 -0400

    homepage setup

--- src/Homepage/HomePage.js
+++ src/Homepage/HomePage.js
@@ -1,12 +1,14 @@
 import React, { Components } from 'react';
-
+import '../../css/App.css';
 class HomePage extends React.Component {
  
   render() {
     return(
       <div>
-        <button>Hello</button>;
+        <button>Hello</button>
       </div>
     ); 
   }
 }
+
+export default HomePage;
