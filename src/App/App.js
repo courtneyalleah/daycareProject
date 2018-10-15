765eb7bd309cc069be495a930f6e63f61b250eff
Courtney <csuper95@gmail.com>
2018-10-15 19:03:10 -0400

    more page setup

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,26 @@
 import React, {Component} from 'react';
 import '../../css/App.css';
+import HomePage from '../Homepage/HomePage';
+import {
+  Route,
+  NavLink,
+  HashRouter
+} from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
+      <HashRouter>
       <div>
-          
+      <h1>Simple SPA</h1>
+          <ul className="header">
+            <li><NavLink to="/">Home</NavLink></li>
+          </ul>
+          <div className="content">
+             <Route path="/" component={HomePage}/>
+          </div>
       </div>
+      </HashRouter>
     );
       
     
